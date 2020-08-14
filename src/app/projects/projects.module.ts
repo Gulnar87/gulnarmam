f53b655ff043431b5819a0337aa4d47e7ea24992
Gulnar Mammadova <gulnarmammadova@Gulnars-MacBook-Pro.local>
2020-08-14 14:31:01 +0200

    removed json ld

--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -5,13 +5,11 @@ import { ProjectItemDetailedComponent } from '../projects/project-item-detailed/
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { ProjectsRoutingModule } from '../projects/projects-routing.module';
 import { NgxJsonLdModule } from '@ngx-lite/json-ld';
-import { JsonLdComponent } from '../json-ld/json-ld.component';
 
 
 @NgModule({
 	declarations: [
     ProjectItemDetailedComponent,
-    JsonLdComponent
 	],
 
 	imports: [
