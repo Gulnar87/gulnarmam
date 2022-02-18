fdbad8cfb309cfbddd2d26d0e06b154aa8051ff9
gumam <gulnar.mammadova@eonics.nl>
2022-02-18 23:08:16 +0100

    update

--- src/app/projects/project.service.ts
+++ src/app/projects/project.service.ts
@@ -2,31 +2,30 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Project } from '../projects/project.model';
-import { Observable } from 'rxjs';
-import { switchMap, take, tap} from 'rxjs/operators';
+
 
 @Injectable()
 export class ProjectService {
-   projectsChanged = new Subject<Project[]>();
-   
-	private	projects : Project[] = [];
+  projectsChanged = new Subject<Project[]>();
 
-	constructor() {}
+  private projects: Project[] = [];
+
+  constructor() { }
 
 
-  getProjects(){
-  	return this.projects.slice();
+  getProjects() {
+    return this.projects.slice();
   }
 
-  getProject(index: number){
+  getProject(index: number) {
     return this.projects[index];
   }
 
-  setProjects(projects: Project[]){
+  setProjects(projects: Project[]) {
     this.projects = projects;
     this.projectsChanged.next(this.projects.slice());
 
   }
-  
+
 
 }
